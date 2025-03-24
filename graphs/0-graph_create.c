412dcf7a8806c2daa52a870ea53a8f0bb7c363ab
popstarmav <cj.johnson2017@gmail.com>
2025-03-24 07:57:18 -0500

    Add graph_create function and graphs.h header file

--- graphs/0-graph_create.c
+++ graphs/0-graph_create.c
@@ -10,16 +10,16 @@
 graph_t *graph_create(void)
 {
 	graph_t *graph;
-	
+
 	/* Allocate memory for the graph structure */
 	graph = malloc(sizeof(graph_t));
 	if (!graph)
 		return (NULL);
-	
+
 	/* Initialize the graph structure */
 	graph->nb_vertices = 0;
 	graph->vertices = NULL;
-	
+
 	return (graph);
 }
 
