a85a2209fb6ae28f993c66c8676f651a082d0f18
popstarmav <cj.johnson2017@gmail.com>
2025-03-25 13:22:38 -0500

    Add graph_add_vertex function to add vertices to a graph

--- graphs/2-graph_add_edge.c
+++ graphs/2-graph_add_edge.c
@@ -3,63 +3,6 @@
 #include <string.h>
 
 /**
- * find_vertex - Finds a vertex in the graph by content
- * @graph: Pointer to the graph
- * @content: String to search for
- *
- * Return: Pointer to the vertex if found, NULL otherwise
- */
-vertex_t *find_vertex(const graph_t *graph, const char *content)
-{
-	vertex_t *vertex;
-
-	if (!graph || !content)
-		return (NULL);
-
-	vertex = graph->vertices;
-	while (vertex)
-	{
-		if (strcmp(vertex->content, content) == 0)
-			return (vertex);
-		vertex = vertex->next;
-	}
-	return (NULL);
-}
-
-/**
- * add_edge_to_vertex - Adds an edge from one vertex to another
- * @src: Source vertex
- * @dest: Destination vertex
- *
- * Return: 1 on success, 0 on failure
- */
-int add_edge_to_vertex(vertex_t *src, vertex_t *dest)
-{
-	edge_t *new_edge, *edge;
-
-	/* Check if edge already exists */
-	edge = src->edges;
-	while (edge)
-	{
-		if (edge->dest == dest)
-			return (1); /* Edge already exists */
-		edge = edge->next;
-	}
-
-	/* Create new edge */
-	new_edge = malloc(sizeof(edge_t));
-	if (!new_edge)
-		return (0);
-
-	new_edge->dest = dest;
-	new_edge->next = src->edges;
-	src->edges = new_edge;
-	src->nb_edges++;
-
-	return (1);
-}
-
-/**
  * graph_add_edge - Adds an edge between two vertices to an existing graph
  * @graph: Pointer to the graph to add the edge to
  * @src: String identifying the vertex to make the connection from
@@ -71,32 +14,85 @@ int add_edge_to_vertex(vertex_t *src, vertex_t *dest)
 int graph_add_edge(graph_t *graph, const char *src, const char *dest,
 		edge_type_t type)
 {
-	vertex_t *src_vertex, *dest_vertex;
+	vertex_t *src_vertex = NULL, *dest_vertex = NULL;
+	vertex_t *tmp = NULL;
+	edge_t *new_edge = NULL, *edge = NULL;
 
 	if (!graph || !src || !dest)
 		return (0);
 
 	/* Find source and destination vertices */
-	src_vertex = find_vertex(graph, src);
-	dest_vertex = find_vertex(graph, dest);
+	tmp = graph->vertices;
+	while (tmp)
+	{
+		if (strcmp(tmp->content, src) == 0)
+			src_vertex = tmp;
+		if (strcmp(tmp->content, dest) == 0)
+			dest_vertex = tmp;
+		tmp = tmp->next;
+	}
 	if (!src_vertex || !dest_vertex)
 		return (0);
 
-	/* Add edge from source to destination */
-	if (!add_edge_to_vertex(src_vertex, dest_vertex))
-		return (0);
+	/* Check if edge already exists */
+	edge = src_vertex->edges;
+	while (edge)
+	{
+		if (edge->dest == dest_vertex)
+			break;
+		edge = edge->next;
+	}
+	if (!edge)
+	{
+		/* Add edge from source to destination at the end of the list */
+		new_edge = malloc(sizeof(edge_t));
+		if (!new_edge)
+			return (0);
+		new_edge->dest = dest_vertex;
+		new_edge->next = NULL;
+
+		if (!src_vertex->edges)
+			src_vertex->edges = new_edge;
+		else
+		{
+			edge = src_vertex->edges;
+			while (edge->next)
+				edge = edge->next;
+			edge->next = new_edge;
+		}
+		src_vertex->nb_edges++;
+	}
 
 	/* If bidirectional, add edge from destination to source */
 	if (type == BIDIRECTIONAL)
 	{
-		if (!add_edge_to_vertex(dest_vertex, src_vertex))
+		/* Check if reverse edge already exists */
+		edge = dest_vertex->edges;
+		while (edge)
 		{
-			/* Remove the edge we just added (cleanup) */
-			edge_t *edge = src_vertex->edges;
-			src_vertex->edges = edge->next;
-			src_vertex->nb_edges--;
-			free(edge);
-			return (0);
+			if (edge->dest == src_vertex)
+				break;
+			edge = edge->next;
+		}
+		if (!edge)
+		{
+			/* Add edge from destination to source at the end of the list */
+			new_edge = malloc(sizeof(edge_t));
+			if (!new_edge)
+				return (0);
+			new_edge->dest = src_vertex;
+			new_edge->next = NULL;
+
+			if (!dest_vertex->edges)
+				dest_vertex->edges = new_edge;
+			else
+			{
+				edge = dest_vertex->edges;
+				while (edge->next)
+					edge = edge->next;
+				edge->next = new_edge;
+			}
+			dest_vertex->nb_edges++;
 		}
 	}
 
