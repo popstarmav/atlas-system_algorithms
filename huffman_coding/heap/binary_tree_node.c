a371ea54591350561e60f0fcd20c14eb44c1e0ad
popstarmav <cj.johnson2017@gmail.com>
2025-04-03 14:19:25 -0500

    function that creates a generic Binary Tree node

--- huffman_coding/heap/binary_tree_node.c
+++ huffman_coding/heap/binary_tree_node.c
@@ -12,16 +12,16 @@
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
 {
 	binary_tree_node_t *new_node = malloc(sizeof(binary_tree_node_t));
-	
+
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-	
+
 	new_node->data = data;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	
+
 	return (new_node);
 }
