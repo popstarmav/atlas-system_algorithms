481fe20b1a26a4eb427500b2e980d589b52164b9
popstarmav <cj.johnson2017@gmail.com>
2025-04-09 09:11:32 -0500

    push

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -2,6 +2,7 @@
 #define HUFFMAN_H
 
 #include <stdlib.h>
+#include "heap/heap.h"
 
 /**
  * struct symbol_s - Stores a char and its associated frequency
@@ -17,6 +18,7 @@ typedef struct symbol_s
 
 /* Function prototypes */
 symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 
 #endif /* HUFFMAN_H */
 
---
+++ huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,77 @@
+#include "huffman.h"
+#include "heap/heap.h"
+#include <stdlib.h>
+
+/**
+ * symbol_cmp - Compares two symbols based on their frequencies
+ * @p1: First symbol (binary_tree_node_t containing symbol_t)
+ * @p2: Second symbol (binary_tree_node_t containing symbol_t)
+ *
+ * Return: Difference between frequencies
+ */
+int symbol_cmp(void *p1, void *p2)
+{
+	binary_tree_node_t *node1, *node2;
+	symbol_t *symbol1, *symbol2;
+
+	node1 = (binary_tree_node_t *)p1;
+	node2 = (binary_tree_node_t *)p2;
+	symbol1 = (symbol_t *)(node1->data);
+	symbol2 = (symbol_t *)(node2->data);
+
+	return (symbol1->freq - symbol2->freq);
+}
+
+/**
+ * huffman_priority_queue - Creates a priority queue for Huffman coding
+ * @data: Array of characters
+ * @freq: Array of frequencies
+ * @size: Size of the arrays
+ *
+ * Return: Pointer to the created min heap (priority queue)
+ */
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+	heap_t *priority_queue;
+	binary_tree_node_t *nested_node;
+	symbol_t *symbol;
+	size_t i;
+
+	/* Create the min heap */
+	priority_queue = heap_create(symbol_cmp);
+	if (!priority_queue)
+		return (NULL);
+
+	/* For each character, create a symbol and add it to the min heap */
+	for (i = 0; i < size; i++)
+	{
+		/* Create a symbol with the character and its frequency */
+		symbol = symbol_create(data[i], freq[i]);
+		if (!symbol)
+		{
+			heap_delete(priority_queue, NULL);
+			return (NULL);
+		}
+
+		/* Create a binary tree node with the symbol as data */
+		nested_node = binary_tree_node(NULL, symbol);
+		if (!nested_node)
+		{
+			free(symbol);
+			heap_delete(priority_queue, NULL);
+			return (NULL);
+		}
+
+		/* Insert the binary tree node into the min heap */
+		if (heap_insert(priority_queue, nested_node) == NULL)
+		{
+			free(symbol);
+			free(nested_node);
+			heap_delete(priority_queue, NULL);
+			return (NULL);
+		}
+	}
+
+	return (priority_queue);
+}
+
