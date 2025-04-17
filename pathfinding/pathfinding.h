ec8f732c6157caa585c6bd75e9d4261882126d88
popstarmav <cj.johnson2017@gmail.com>
2025-04-17 10:59:11 -0500

    push

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -13,15 +13,17 @@
  */
 typedef struct point_s
 {
-	int x;
-	int y;
+    int x;
+    int y;
 } point_t;
 
 /* Function prototypes */
 queue_t *backtracking_array(char **map, int rows, int cols,
-			point_t const *start, point_t const *target);
+            point_t const *start, point_t const *target);
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
-			vertex_t const *target);
+            vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
+            vertex_t const *target);
 
 #endif /* PATHFINDING_H */
 
