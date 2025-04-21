c0267968204e5322e77ca9dc28fc02ab853aa28e
popstarmav <cj.johnson2017@gmail.com>
2025-04-20 19:06:12 -0500

    Add function to traverse N-ary tree and track maximum depth

--- nary_trees/nary_trees.h
+++ nary_trees/nary_trees.h
@@ -24,6 +24,7 @@ typedef struct nary_tree_s
 /* Function prototypes */
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str);
 void nary_tree_delete(nary_tree_t *tree);
+size_t nary_tree_traverse(nary_tree_t const *root, void (*action)(nary_tree_t const *node, size_t depth));
 
 #endif /* NARY_TREES_H */
 
